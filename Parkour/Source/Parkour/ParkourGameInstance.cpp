2eb5462d147ecb4e973685655c1454f42e7cb4b5
Samuel Pattuzzi <sam@pattuzzi.it>
2017-07-04 13:02:48 +0100

    Create a game class instance and log out.

---
+++ Parkour/Source/Parkour/ParkourGameInstance.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ParkourGameInstance.h"
+
+void UParkourGameInstance::StartGameInstance()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game instance started."));
+}
